286ac3f7fce920a27c6265b89e707ce31d7902c9
Kyle Fritz <kyle.p.fritz@gmail.com>
2010-12-15 20:57:28 -0600

    made cgi saver script

---
+++ save.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import cgi
+import cgitb;cgitb.enable()
+from datetime import datetime
+
+form=cgi.FieldStorage()
+email=form.getvalue("email","no email")
+
+f=open('../emails/emails.txt','a')
+f.write("%s\t%s\n"%(datetime.now(),email))
+f.close()
+
+print "Content-type:text/html"
+print
+print "<html>We've got you're email as %s. We'll talk to you soon.</html>"%email
+
