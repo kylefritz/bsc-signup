ace6ba124bae83454672f78031395ef5f0916dde
Kyle Fritz <kyle.p.fritz@gmail.com>
2010-12-15 21:46:06 -0600

    launch

--- site.js
+++ site.js
@@ -1,3 +1,40 @@
 $(function(){
+	var $f=$('form');
 
+
+	var $e=$('input[name=email]')
+	var $s=$('input[type=submit]');
+	var emailregex=/\w+@\w+\.\w+/;
+	var enabled='enabled';
+
+	var validate=function(){
+		if(emailregex.test($e.val()))
+		{
+			$s.addClass(enabled);
+		}else{
+			$s.removeClass(enabled);
+		}
+	};
+
+	$e.keydown(validate).change(validate);
+
+	$f.submit(function(e){
+		e.preventDefault();
+		validate();
+		if(!$s.hasClass(enabled))
+		{
+			alert('Enter your email, please.');
+			return;
+		}
+		var $prev=$f.hide().prev().hide();
+		$('h1').animate({fontSize:'200pt'});
+		$.post(
+			$f.attr('action'),
+			$f.serialize(),
+			function(){
+				$('h1').animate({fontSize:'80pt'},function(){
+					$prev.html('we got you covered.').fadeIn('fast');
+				});
+			});
+	});
 });
